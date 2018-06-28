e8f480ab2fd218d4687ff2155ff6b9c884c445da
Akshay Nailwal <akshaynailwal999@gmail.com>
2018-06-28 10:10:59 +0530

    welcomepage

---
+++ welcomepage.php
@@ -0,0 +1,71 @@
+<!Doctype html>
+
+<html>
+	<head>
+
+		<title>Welcome Page</title>
+
+		<meat name="viewport" content="width=device-width initial-scale=1, shrink-to-fit=no">
+		<meta charset ="UTF-8">
+		
+		<!-- CSSReference -->
+		<link rel = "stylesheet" href ="style2.css" type ="text/css">
+
+	</head>
+	
+	<!-- BODY SECTION ---------------------------------------------------------------------------------- -->
+	
+	<body>
+			
+			<!-- HEADER SECTION ---------------------------------------------------------------------------------- -->	
+			
+
+			<header>
+				<div class="helements">
+					<a href ="#" class ="element1">Home</a>
+					<a href ="#" class ="element2">Info.</a>
+					<a href ="search.php" class ="element3">Parkings</a>
+					<a href ="booking.php" class ="element4">Booking</a>
+					<a href ="contactus.php" class ="element5">Contact us</a>
+				</div>
+			</header>
+			
+			
+			<!-- HEADING SECTION ---------------------------------------------------------------------------------- -->
+			
+			<div id="fheading">
+			
+				<h1>WELCOME TO ACPS</h1>
+				
+				<input id = "Welcomebutton" type = "button" name = "Gotomain" value = "Go to main &#8677;" 
+					onclick ="location.href ='booking.php'"></input>
+			</div>
+
+			<!-- FOOTER SECTION ---------------------------------------------------------------------------------- -->
+			
+			
+			<div>
+				<footer>
+					<div class = "felementstxt">
+						&#169;  Copyright : ACPS corps. ltd.</br>
+								Since 2017- until dead
+					</div>
+					<div class="felementsicons">
+					
+						<a href ="http://www.facebook.com"><div id="facebook">
+							f
+						</div></a>
+						<a href ="http://www.twitter.com"><div id="twitter">
+							t
+						</div></a>
+						<a href ="http://www.google.com/gmail"><div id="gmail">
+							G+
+						</div></a>
+						
+					</div>
+				</footer>
+			</div>
+			
+	</body>
+
+</html>
