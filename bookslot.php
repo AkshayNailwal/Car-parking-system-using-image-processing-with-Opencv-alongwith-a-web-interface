b31bee73b06143d175bcda50a452283a5b4a9d83
Akshay Nailwal <akshaynailwal999@gmail.com>
2018-06-28 10:14:10 +0530

    slotbooking

---
+++ bookslot.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>PARKING SYSTEM</title>
+<meat name="viewport" content="width=device-width initial-scale=1, shrink-to-fit=no">
+		
+		<!-- CSSReference -->
+		<link rel = "stylesheet" href ="style.css" type ="text/css">
+
+</head>
+	<!-- BODY SECTION ---------------------------------------------------------------------------------- -->
+	
+<body>
+			<!-- HEADER SECTION ---------------------------------------------------------------------------------- -->	
+			
+
+				<header>
+				<div class="helements">
+					<a href ="welcomepage.php" class ="element1">Home</a>
+					<a href ="#" class ="element2">Info.</a>
+					<a href ="search.php" class ="element3">Parkings</a>
+					<a href ="booking.php" class ="element4">Booking</a>
+					<a href ="contactus.php" class ="element5">Contact us</a>
+				</div>
+			</header>
+
+			<!-- HEADING SECTION ---------------------------------------------------------------------------------- -->
+			
+			<div id="fheading" align ="center"><h1>Sample Parking Layout</h1></div>			
+				
+			<!-- -------------------------------PARKING SLOT SECTION(CHECKBOXES) ---------------------------------- -->
+			
+			
+			
+			<!-- SLOT SECTION (    1    )-------------------------------------------------------------------- -->
+<FORM method="post" action="submitted.php" enctype="multipart/form-data">
+	<div>
+				<ul style="list-style-type:none">
+					<li><div class ="checkbox-container" align="center">
+						<ul>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="1"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="2"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="3"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="4"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="5"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="6"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="7"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="8"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="9"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="10"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="11"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="12"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="13"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="14"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="15"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="16"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="17"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="18"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="19"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="20"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="21"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="22"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="23"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="24"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="25"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="26"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="27"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="28"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="29"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="30"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="31"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="32"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="33"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="34"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="35"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="36"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="37"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="38"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="39"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="40"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="41"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="42"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="43"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="44"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="45"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="46"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="47"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="48"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="49"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="50"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="51"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="52"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="53"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="54"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="55"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="56"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="57"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="58"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="59"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="60"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="61"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="62"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="63"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="64"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="65"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="66"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="67"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="68"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="69"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="70"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="71"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="72"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="73"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="74"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="75"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="76"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="77"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="78"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="79"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="80"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="81"></input></div></br></br>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="82"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="83"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="84"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="85"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="86"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="87"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="88"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="89"></input></div>
+							<div class = "checkbox" ><input class = "checkclass"  type="checkbox" name = "book[]" value="90"></input></div></br></br>
+						</ul>
+					</div></li></br>
+				<div align="center">
+				<button id = "seatbooked" type ="submit" onclick ="window.location.href='submitted.php'"  name ="Submit">Submit
+				</button></div>	
+				</ul>
+			</div>		
+</FORM>
+			
+			<!-- FOOTER SECTION ---------------------------------------------------------------------------------- -->
+			
+			
+			<div>
+				<footer>
+					<div class = "felementstxt">
+						&#169;  Copyright : ACPS corps. ltd.</br>
+								Since 2017- until dead
+					</div>
+					<div class="felementsicons">
+					
+						<a href ="http://www.facebook.com"><div id="facebook">
+							f
+						</div></a>
+						<a href ="http://www.twitter.com"><div id="twitter">
+							t
+						</div></a>
+						<a href ="http://www.google.com/gmail"><div id="gmail">
+							G+
+						</div></a>
+						
+					</div>
+				</footer>
+			</div>
+		
+<?php 
+$con=mysqli_connect("127.0.0.1","root","","Project1");
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$owner="";
+$car="";
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+$owner = $_POST["ownername"];
+$car = $_POST["carnumber"];
+}
+$sql = "INSERT INTO parking(owner_name,car_number) VALUES ('$owner','$car')";
+mysqli_query($con, $sql);
+mysqli_close($con);
+?>
+	</body>
+</html>
